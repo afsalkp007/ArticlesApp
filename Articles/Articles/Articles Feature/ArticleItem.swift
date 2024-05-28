caa1d2d50a01fbaa39a6e85697c2e65941cca7f5
Afsal <afsalkp.mohammed@gmail.com>
2024-05-28 15:38:40 +0400

    Adding .gitignore file
    Created Article item struct

---
+++ Articles/Articles/Articles Feature/ArticleItem.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleItem.swift
+//  Articles
+//
+//  Created by Afsal on 28/05/2024.
+//
+
+import Foundation
+
+struct ArticleItem {
+  let title: String
+  let byline: String
+  let date: Date
+  let imageURL: URL
+}
---
+++ Articles/Articles/Articles Feature/ArticleLoader.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleLoader.swift
+//  Articles
+//
+//  Created by Afsal on 28/05/2024.
+//
+
+import Foundation
+
+enum ArticleResult {
+  case success([ArticleItem])
+  case failure(Error)
+}
+
+protocol ArticleLoader {
+  func load(completion: (ArticleResult) -> Void)
+}
