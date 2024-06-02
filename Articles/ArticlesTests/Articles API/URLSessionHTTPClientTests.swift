38e7ee2996647e8bcb9cd8238b5368260a992dea
Afsal <afsalkp.mohammed@gmail.com>
2024-06-02 16:57:10 +0400

    move URL creation to a shared helper method

--- Articles/ArticlesTests/Articles API/URLSessionHTTPClientTests.swift
+++ Articles/ArticlesTests/Articles API/URLSessionHTTPClientTests.swift
@@ -39,14 +39,12 @@ class URLSessionHTTPClientTests: XCTestCase {
   }
   
   func test_getFromURL_deliversErrorOnClientError() {
-    URLProtocolStub.startInterceptingRequests()
-    let url = URL(string: "http://any-url.com")!
     let error = NSError(domain: "any error", code: 1)
     URLProtocolStub.stub(data: nil, response: nil, error: error)
         
     let exp = expectation(description: "Wait for completion")
     
-    makeSUT().get(from: url) { result in
+    makeSUT().get(from: anyURL()) { result in
       switch result {
       case let .failure(receivedError as NSError):
         XCTAssertEqual(receivedError.domain, error.domain)
@@ -63,7 +61,7 @@ class URLSessionHTTPClientTests: XCTestCase {
   }
   
   func test_getFromURL_performsGETRequestWithURL() {
-    let url = URL(string: "http://any-url.com")!
+    let url = anyURL()
     
     let exp = expectation(description: "Wait for request")
 
@@ -84,6 +82,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     let sut = URLSessionHTTPClient()
     trackForMemoryLeak(sut, file: file, line: line)
     return sut
+  }
+  
+  private func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
   }
   
   private class URLProtocolStub: URLProtocol {
